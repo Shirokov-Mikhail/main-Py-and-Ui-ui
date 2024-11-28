4f02378a2f0779f264dd40fb8d207e19518d548f
itcube22 <104897682+itcube22@users.noreply.github.com>
2024-11-28 18:46:16 +0700

    G

--- main.py
+++ main.py
@@ -1,8 +1,55 @@
-def main():
-    print('My first git program')
-    print('красный это плохо!!')
-    print('зеленый хорошо но моя прога говно')
+import random
+import sys
+
+from PyQt6 import uic
+from PyQt6.QtGui import QPainter, QColor
+from PyQt6.QtWidgets import QWidget, QApplication
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLineEdit, QApplication, QPushButton, \
+    QLabel, QLCDNumber, QCheckBox, QButtonGroup, QDoubleSpinBox, QPlainTextEdit, QStatusBar, QListWidget
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        uic.loadUi('UI.ui', self)
+        self.setWindowTitle('Рисование')
+        self.do_paint = True
+        self.pushButton: QPushButton
+        self.pushButton.clicked.connect(self.paint)
+
+    # Метод срабатывает, когда виджету надо
+    # перерисовать свое содержимое,
+    # например, при создании формы
+    def paintEvent(self, event):
+        if self.do_paint:
+            # Создаем объект QPainter для рисования
+            qp = QPainter()
+            # Начинаем процесс рисования
+            qp.begin(self)
+            self.draw_flag(qp)
+            # Завершаем рисование
+            qp.end()
+        self.do_paint = False
+
+    def draw_flag(self, qp):
+        # Задаем кисть
+        qp.setBrush(QColor(255, 0, 0))
+        # Рисуем прямоугольник заданной кистью
+        d = random.randint(10, 300)
+        x = random.randint(10, 300)
+        y = random.randint(10, 300)
+        qp.drawEllipse(x, y, d, d)
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
